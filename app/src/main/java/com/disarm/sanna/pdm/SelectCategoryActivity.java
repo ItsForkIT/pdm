6f65efeaf5243f6215d7939c9e2383647193eff7
archie94 <arkaprava94@gmail.com>
2017-03-03 23:09:47 +0530

    Save crash logs in a file on application crash
    A thread implemented to handle uncaught exceptions and write the stack
    trace in a file.
    
    Fixes #42

--- app/src/main/java/com/disarm/sanna/pdm/SelectCategoryActivity.java
+++ app/src/main/java/com/disarm/sanna/pdm/SelectCategoryActivity.java
@@ -33,7 +33,12 @@ import com.disarm.sanna.pdm.location.MLocation;
 import com.nextgis.maplib.util.SettingsConstants;
 
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import belka.us.androidtoggleswitch.widgets.BaseToggleSwitch;
 import belka.us.androidtoggleswitch.widgets.BaseToggleSwitch.OnToggleSwitchChangeListener;
@@ -160,6 +165,31 @@ public class SelectCategoryActivity extends AppCompatActivity{
                         e.printStackTrace();
                     }
                     startActivity(intentGIS);
+                }
+            }
+        });
+
+        // Save crash logs in a file every time the application crashes
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                Calendar cal = Calendar.getInstance();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+                String filename = SettingsConstants.DMS_PATH + "crashlogs/" +
+                        sdf.format(cal.getTime())+".txt";
+
+                PrintStream writer;
+                try {
+                    writer = new PrintStream(filename, "UTF-8");
+                    writer.println(e.getClass() + ": " + e.getMessage());
+                    for (int i = 0; i < e.getStackTrace().length; i++) {
+                        writer.println(e.getStackTrace()[i].toString());
+                    }
+
+                } catch (FileNotFoundException | UnsupportedEncodingException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
                 }
             }
         });
