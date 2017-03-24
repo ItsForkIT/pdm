f145c11bcd9c185911896c0fac3cb3d4ab3ec0ed
Naman <namanmehta1997@gmail.com>
2017-03-24 19:35:13 +0530

    State change finder added

--- app/src/main/java/com/disarm/sanna/pdm/Opp/Parameter.java
+++ app/src/main/java/com/disarm/sanna/pdm/Opp/Parameter.java
@@ -4,10 +4,12 @@ package com.disarm.sanna.pdm.Opp;
  * Created by naman on 24/3/17.
  */
 
-public class parameter {
+public class Parameter {
     public static int maximum_AP_on = 600; //10 minutes
     public static int minimum_wait_AP = 10; //10 seconds
     public static int maximum_limit_wait_AP = 120; //2 minutes
     public static int alpha=2; //STA switch parameter
-    public static double ws=1/40;
+    public static double beta=1/2; //AP switch parameter
+    public static double ws=1/40; //STA switch parameter
+    public static double wa=1/20; //AP switch parameter
 }
